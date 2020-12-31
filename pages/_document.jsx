ab152a720b7abc8b899c5a05e8df9509e6d96195
Dijana Pavlovic <dijana.pavlovic992@gmail.com>
2020-12-31 11:41:48 +0100

    Update link of the favicon.ico.

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -15,7 +15,7 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Mulish:wght@200;300;400;500;600;700;800;900&display=swap"
             rel="stylesheet"
           />
-          <link rel="shortcut icon" href="/favicon.ico" />
+          <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
         </Head>
         <body>
           <Main />
