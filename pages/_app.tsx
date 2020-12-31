3e2370a1ea5920eb7932677b768170fbd5ecb31c
Dijana Pavlovic <dijana.pavlovic992@gmail.com>
2020-12-31 11:37:32 +0100

    Revert changes.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,16 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui";
 import { ThemeProvider } from "theme-ui";
-import Head from "next/head";
 import theme from "../theme";
 import Nav from "../src/components/nav";
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
-      <Head>
-        <link rel="shortcut icon" href="/favicon.ico" />
-      </Head>
-      <ThemeProvider theme={theme}>
-        <>
-          <Nav />
-          <Component {...pageProps} />
-        </>
-      </ThemeProvider>
-    </>
+    <ThemeProvider theme={theme}>
+      <>
+        <Nav />
+        <Component {...pageProps} />
+      </>
+    </ThemeProvider>
   );
 }
