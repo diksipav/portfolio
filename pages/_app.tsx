0a07b8557057e03b3e3f6f0071fb1ed02c2b49c0
Dijana Pavlovic <dijana.pavlovic992@gmail.com>
2020-12-31 01:05:28 +0100

    Add React fragment instead div.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,10 +7,10 @@ import Nav from "../src/components/nav";
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
-      <div>
+      <>
         <Nav />
         <Component {...pageProps} />
-      </div>
+      </>
     </ThemeProvider>
   );
 }
